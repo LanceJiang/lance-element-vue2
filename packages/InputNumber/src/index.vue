d704e19ded349fae40dd85a56f74f811775775f7
lanceJiang <lancejiangwei@qq.com>
2022-10-25 11:12:27 +0800

    fix: inputNumber 组件调整

--- packages/InputNumber/src/index.vue
+++ packages/InputNumber/src/index.vue
@@ -13,17 +13,12 @@ export default {
   },
   render () {
     const { prefix, suffix, prop, controlsPosition, ...props } = this.$attrs
-    const { inputNumberSize } = this
+    const { inputNumberSize, value } = this
     const $slots = this.$slots
     const onEvents = this.$listeners
-    console.error(inputNumberSize, 'inputNumberSize')
-    // todo
-    /* //  wrap: el-input--prefix el-input--suffix
-    //  addon_prefix: el-input__icon el-input__prefix
-    //  addon_suffix: el-input__icon el-input__suffix */
 
     const _prefix = prefix ? <span class='ad-addon ad-input-number__prefix'>{prefix}</span> : ''
-    const _suffix = suffix ? <span class='ad-addon ad-input-number__suffix'>{suffix}</span> : '' // el-input__suffix-inner
+    const _suffix = suffix ? <span class='ad-addon ad-input-number__suffix'>{suffix}</span> : ''
     return <div class={`ad-input-number ad-input-number--${inputNumberSize} el-input el-input-group
      ${(_prefix || $slots.prefix) ? 'ad-input-number--prefix' : ''}
      ${(_suffix || $slots.suffix) ? 'ad-input-number--suffix' : ''}
@@ -33,7 +28,8 @@ export default {
         props={{ ...props }}
         size={inputNumberSize}
         controlsPosition={controlsPosition || 'right'}
-        value={this.localValue}
+        // value={this.localValue}
+        value={value}
         on={onEvents}/>
       {$slots.suffix || _suffix}
     </div>
@@ -42,14 +38,14 @@ export default {
     return {}
   },
   computed: {
-    localValue () {
-      const { prop } = this.$attrs
-      const { value, params } = this
-      if (prop && params) {
-        return params[prop]
-      }
-      return value
-    },
+    // localValue () {
+    //   const { prop } = this.$attrs
+    //   const { value, params } = this
+    //   if (prop && params) {
+    //     return params[prop]
+    //   }
+    //   return value
+    // },
     inputNumberSize() {
       return this.$attrs.size || (this.$ELEMENT || {}).size
     }
